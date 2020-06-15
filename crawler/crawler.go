299ff12395994430acb88433c9f4d5c7e209a534
zhangwei412827@163.com <>
2020-06-15 13:00:47 +0800

    add blog.cache

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -53,7 +52,12 @@ func (cr *Crawler) Start() {
 	writeToCacheFile(cr.Buf)
 }
 func (cr *Crawler) craw(b *blog, pageNum int, wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+		wg.Done()
+	}()
 	var addr string
 	if pageNum == 1 {
 		addr = b.Address
@@ -75,17 +79,17 @@ func (cr *Crawler) craw(b *blog, pageNum int, wg *sync.WaitGroup) {
 	// Request the HTML page.
 	res, err := http.Get(addr)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		panic(fmt.Sprintf("status code error: %d %s", res.StatusCode, res.Status))
 	}
 
 	// Load the HTML document
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	posts := doc.Find(b.PostStyle)
@@ -107,31 +111,6 @@ func (cr *Crawler) craw(b *blog, pageNum int, wg *sync.WaitGroup) {
 		}
 
 		timeStr := s.Find(b.TimeStyle).Text()
-		//if timeStr == "" {
-		//	//TODO get first blog
-		//	noNewBlog = true
-		//	return
-		//}
-		//
-		//time, err := formatTime(timeStr)
-		//if err != nil {
-		//	//TODO get first blog
-		//	noNewBlog = true
-		//	return
-		//}
-		//pTime, err := formatTime(b.PresentTime)
-		//if err != nil {
-		//	panic("Convert present time error.")
-		//}
-		//if time.Before(pTime) {
-		//	noNewBlog = true
-		//	return
-		//}
-
-		//record the present time
-		//if authorPresent[b.Author].IsZero() {
-		//	authorPresent[b.Author] = time
-		//}
 
 		address, _ := s.Find(b.TitleStyle).Attr("href")
 		author := b.Author
@@ -181,23 +160,6 @@ func replacePageNum(uri, newPage string) string {
 	return uri
 }
 
-func getNum(path string) (int, error) {
-	numRg := regexp.MustCompile(`(\d+)`)
-	if !numRg.MatchString(path) {
-		return 0, fmt.Errorf("Compile path error.")
-	}
-	num := numRg.FindAllString(path, -1)
-	if len(num) >= 1 {
-		n, _ := strconv.Atoi(num[0])
-		return n, nil
-	}
-	return 0, fmt.Errorf("Compile path error.")
-}
-func checkNum(pageNum string) bool {
-	numRg := regexp.MustCompile(`[1-9]\d+|^[0-9]$`)
-	isNum := numRg.MatchString(pageNum)
-	return isNum
-}
 func initPageRule(b *blog, doc *goquery.Document) {
 	if b.PageRule == "" {
 		if ps := doc.Find(b.PageStyle); ps.Length() > 1 {
@@ -210,31 +172,11 @@ func initPageRule(b *blog, doc *goquery.Document) {
 		}
 	}
 }
-func formatTime(timeStr string) (time.Time, error) {
-	exp, err := regexp.Compile(`^\d{4}([年月日/.]{1})\d{1,2}([年月日/.]{1})\d{1,2}([年月日/.])?$`)
-	if err != nil || !exp.MatchString(timeStr) {
-		return time.Now(), err
-	}
-	rep := []string{"年", "月", "日", "/", "."}
-
-	for _, r := range rep {
-		timeStr = strings.Replace(timeStr, r, "-", -1)
-	}
-	if timeStr[len(timeStr)-1:] == "-" {
-		timeStr = timeStr[:len(timeStr)-1]
-	}
-	t, err := time.Parse("2006-1-2", timeStr)
-
-	if err != nil {
-		return time.Now(), err
-	}
-	return t, nil
-}
 func writeToCacheFile(buf *bytes.Buffer) {
 	f, err := os.OpenFile("./blog.cache", os.O_APPEND|os.O_WRONLY, 0644)
 
 	if err != nil {
-		log.Fatal("Write to cache file error: %v", err)
+		panic(fmt.Sprintf("Write to cache file error: %v", err))
 	}
 	defer f.Close()
 	_, err = f.Write(buf.Bytes())
